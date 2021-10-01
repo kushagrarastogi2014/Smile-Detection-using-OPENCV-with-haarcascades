7d9e84d46140886b68d08768a490888d90767e94
kakkarmanan <mr.manankakkar@gmail.com>
2021-10-01 22:23:56 +0530

    file add

---
+++ root.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:zoom/theme/colors.dart';
+
+List bottomItems = [
+  MaterialCommunityIcons.microphone,
+  MaterialCommunityIcons.video,
+  Ionicons.ios_share,
+  Entypo.slideshare,
+  Icons.more_horiz
+];
+List textItems = ["Mute", "Stop Video", "Share Screen", "Participants", "More"];
+List colorItems = [
+  Colors.grey,
+  Colors.grey,
+  Colors.green,
+  Colors.grey,
+  Colors.grey,
+];
+List sizedItems = [
+  30.0,
+  30.0,
+  30.0,
+  28.0,
+  30.0,
+];
+
+List actionSheetItems = [
+  "Raise Hand",
+  "Chat",
+  "Meeting Settings",
+  "Minimize Meeting",
+  "Virtual Background",
+  "Disconnect Audio",
+  "👏   👍   💔   🤣   😮   🎉"
+];
